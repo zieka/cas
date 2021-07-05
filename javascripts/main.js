04b094588189debcf91d0a929ec82e9404d1ba00
Misagh <mm1844@gmail.com>
2021-07-05 17:38:48 +0400

    allow copy button

--- javascripts/main.js
+++ javascripts/main.js
@@ -446,3 +446,25 @@ function previous(id) {
 
   $(`#${id} thead tr`).show();
 }
+
+
+let codes = document.querySelectorAll('.highlight > pre > code .rouge-code pre');
+let countID = 0;
+codes.forEach((code) => {
+
+  code.setAttribute("id", "code" + countID);
+  
+  let btn = document.createElement('button');
+  btn.innerHTML = "Copy";
+  btn.className = "btn-copy-code";
+  btn.setAttribute("data-clipboard-action", "copy");
+  btn.setAttribute("data-clipboard-target", "#code" + countID);
+  
+  let div = document.createElement('div');
+  div.className = "div-code-button";
+  div.appendChild(btn);
+  
+  code.before(div);
+  countID++;
+}); 
+new ClipboardJS('.btn-copy-code');
